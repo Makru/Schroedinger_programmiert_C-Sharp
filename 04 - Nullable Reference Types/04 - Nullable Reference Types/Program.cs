a11ee11328dd40171ed503fd71f31d190ea9762f
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-10-28 14:21:54 +0100

    Kapitel 4 added

---
+++ 04 - Nullable Reference Types/04 - Nullable Reference Types/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+#nullable enable
+namespace _04___Nullable_Reference_Types
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string myString = null;
+            Console.WriteLine(myString.Length);
+            Console.ReadKey();
+        }
+    }
+    
+}
