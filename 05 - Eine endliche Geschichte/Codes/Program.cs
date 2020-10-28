801353eb1133ae4bf6c6b8758e037d16872c41b1
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-10-28 14:33:00 +0100

    Kapitel 5 added

---
+++ 05 - Eine endliche Geschichte/Codes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codes
+{
+    class Program
+    {
+        enum Weekday
+        {
+            Monday = 1, Tuesday = 2, Wednesday = 4, Thursday = 8, Friday = 16, Saturday = 32, Sunday = 64
+        }
+        enum WoWVolk
+        {
+            Menschen, Zwerge, Gnome, Nachtelfen, Draenei, Worgen, Orcs, Untote, Tauren, Trolle, Blutelfen, Goblin, Pandaren
+        }
+        enum WoWKlasse
+        {
+            Druide, Hexenmeister, Jaeger, Krieger, Magier, Paladin, Priester, Schamane, Schurke, Todesritter, Moench
+        }
+
+        static void Main(string[] args)
+        {
+            Weekday myDay = Weekday.Friday;
+            int weekday = (int)myDay;
+            //Weekday anotherDay = (Weekday)7000;	// Auch das würde funktionieren.
+            Console.WriteLine(weekday);
+            Console.WriteLine(myDay.ToString());
+
+            myDay.ToString(); // Umwandeln in den Text
+            Weekday theDay = (Weekday)Enum.Parse(typeof(Weekday), "Monday");
+            Console.WriteLine(theDay);
+
+            WoWVolk meinVolk = WoWVolk.Orcs;
+            if (meinVolk == WoWVolk.Orcs)
+                Console.WriteLine("Dieses Volk spielt Schrödinger");
+            else
+                Console.WriteLine(meinVolk);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 05 - Eine endliche Geschichte/Codes02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codes02 {
+	class Program {
+		enum Weekday {
+			Monday = 1, Tuesday = 2, Wednesday = 4, Thursday = 8, Friday = 16, Saturday = 32, Sunday = 64
+		}
+		static void Main(string[] args) {
+			Console.WriteLine("Bitte englischen Wochentag eingeben: ");
+			string day = Console.ReadLine();
+			Weekday myDay = (Weekday)Enum.Parse(typeof(Weekday), day);
+			Console.WriteLine("Der eingegebene Wochentag im Enum-Typ ist {0}:", myDay);
+            Console.ReadKey();
+		}
+	}
+}
