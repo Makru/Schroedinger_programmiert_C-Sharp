05c3fd5c80141903ae4b94ae648c7ae1ea528765
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-11-11 15:09:42 +0100

    Kapitel 22 added

---
+++ 22 MVVM/HeuteAbend10/DelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HeuteAbend10 {
+	public class DelegateCommand<T> : ICommand {
+		Predicate<T> canExecuteHdl { get; set; }
+		Action<T> executeHdl { get; set; }
+
+		public DelegateCommand(Action<T> executeHdl, Predicate<T> canExecuteHdl) {
+			this.canExecuteHdl = canExecuteHdl;
+			if (executeHdl == null)
+				throw new ArgumentNullException("executeHdl", "Please specifiy the command.");
+			this.executeHdl = executeHdl;
+		}
+
+		public void RaiseCanExecuteChanged() {
+			if (this.CanExecuteChanged != null)
+				this.CanExecuteChanged(this, null);
+		}
+
+		public bool CanExecute(object parameter) {
+			return canExecuteHdl == null || canExecuteHdl((T)parameter) == true;
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		public void Execute(object parameter) {
+			executeHdl((T)parameter);
+		}
+	}
+}
---
+++ 22 MVVM/HeuteAbend10/MainPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeuteAbend10 {
+	public class MainPageViewModel : ModelBase {
+		Random rnd = new Random(DateTime.Now.Millisecond);
+		string text;
+
+		public string Text {
+			get { return text; }
+			set {
+				if (text != value) {
+					text = value;
+					this.OnPropertyChanged();
+				}
+			}
+		}
+
+		public MainPageViewModel() {
+			this.Spielen = new DelegateCommand<string>(
+					(prob) => Evaluate(int.Parse(prob), "Heute wird WoW gespielt!", "Nein, mit dem spielen wird es heute nichts."),
+					(prob) => CheckCanExecute(prob)
+				);
+			this.Freundin = new DelegateCommand<string>(
+				(prob) => Evaluate(int.Parse(prob), "Freundin sucht aus!", "Ich überleg nochmal"),
+				CheckCanExecute
+				);
+			this.Fortgehen = new DelegateCommand<string>(
+				(prob) => Evaluate(int.Parse(prob), "Wir gehen aus!", "Nein, ausgehen wird nichts."),
+				null
+			);
+		}
+
+		public DelegateCommand<string> Freundin { get; set; }
+		public DelegateCommand<string> Fortgehen { get; set; }
+		public DelegateCommand<string> Spielen { get; set; }
+
+		bool CheckCanExecute(string prob) {
+			return true;
+		}
+
+		void Evaluate(int probability, string okText, string nOkText) {
+			int rndNumber = rnd.Next(1, probability + 1);
+			if (rndNumber == probability)
+				this.Text = okText;
+			else
+				this.Text = nOkText;
+
+			this.Freundin.RaiseCanExecuteChanged();
+			this.Fortgehen.RaiseCanExecuteChanged();
+			this.Spielen.RaiseCanExecuteChanged();
+		}
+	}
+}
