4657f9ec9a18425520477fcc02c811d08efb0273
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-11-11 15:15:00 +0100

    Kapitel 1-1 added

---
+++ 22 MVVM - 1 - 1/ButtonBinding/ButtonBinding/MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonBinding {
+	public class MainPageViewModel : ModelBase {
+		string message;
+		public string Message {
+			get { return message; }
+			set {
+				if (this.message != value) {
+					this.message = value;
+					this.OnPropertyChanged();
+				}
+			}
+		}
+
+		string text;
+		public string Text {
+			get { return this.text; }
+			set {
+				if (this.text != value) {
+					this.text = value;
+					this.OnPropertyChanged();
+					this.SayHelloCommand.RaiseCanExecuteChanged();
+				}
+			}
+		}
+
+		public MainPageViewModel() {
+			this.SayHelloCommand = new DelegateCommand<string>(
+				(s) => this.Message = string.Format("Hallo {0}", this.Text),
+				(s) => !String.IsNullOrEmpty(this.Text)
+				);
+		}
+		public DelegateCommand<string> SayHelloCommand { get; set; }
+	}
+}
---
+++ 22 MVVM - 1 - 1/ButtonBinding/ButtonBindingUnitTests/MainPageViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using ButtonBinding;
+
+namespace ButtonBindingUnitTests {
+    [TestClass]
+    public class MainPageViewModelTests {
+        [TestMethod]
+        public void ConstructorTest() {
+            MainPageViewModel vm = new MainPageViewModel();
+            Assert.IsNotNull(vm.SayHelloCommand);
+        }
+
+        [TestMethod]
+        public void MessageFireEventTest() {
+            MainPageViewModel vm = new MainPageViewModel();
+            string message = "Hallo Test";
+            bool eventFired = false;
+            vm.PropertyChanged += (o, e) => eventFired = e.PropertyName == "Message";
+            vm.Message = message;
+            Assert.IsTrue(eventFired);
+            Assert.AreEqual(message, vm.Message);
+        }
+    }
+}
